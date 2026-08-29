1410ec7f7cdabdfeaec57b5dcd5148b5bc1f6c64
arslane18 <ami54500@gmail.com>
2026-08-29 21:29:01 +0200

    save in hive format using pyspark

--- dags/ingestion.py
+++ dags/ingestion.py
@@ -1,6 +1,8 @@
 from airflow.sdk import dag, task
 from ingestion_utils import fetch_data_from_api, retrieve_boundaries_years
-
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 
 @dag(
     schedule=None,
@@ -25,8 +27,22 @@ def simple_extraction():
         with open(f"data/eco2mix-regional-cons-def{str(year)}.parquet", "wb") as wb:
             wb.write(year_of_data.content)
 
+    @task
+    def split_hive_format(root_folder_name: str):
+        '''
+        Split in hive format /year= /month= /day= for data storage'''
+        spark = SparkSession.builder.appName("eco2mix-bronze").getOrCreate()
+        df = spark.read.parquet("data/eco2mix-regional-cons-def*.parquet")
+        df_with_parts = (
+            df
+            .withColumn("year", F.year("date_heure"))
+            .withColumn("month", F.month("date_heure"))
+            .withColumn("day", F.dayofmonth("date_heure"))
+        )
+        df_with_parts.write.partitionBy("year", "month", "day").parquet(f"data/bronze/{root_folder_name}")
+
     years = compute_years()
-    fetch_year.expand(year=years)
+    fetch_year.expand(year=years) >> split_hive_format("eco2mix-regional-cons-def")
 
 
 simple_extraction()

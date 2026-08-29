20616c801fcb163d7385833a99e271dcdac91f50
arslane18 <ami54500@gmail.com>
2026-08-29 19:33:29 +0200

    enabled partitions by year but no parallelism yet (SQLite doesnt like it)

--- dags/ingestion.py
+++ dags/ingestion.py
@@ -12,15 +12,21 @@ def simple_extraction():
     Simple ingestion from Elec API 
     """
 
-    @task()
-    def extract_data_from_eco2mix():
-        boundaries_years = retrieve_boundaries_years()
-        start_year, end_year = boundaries_years["start_year"], boundaries_years["end_year"]
-        for curr_year in range(start_year + 1, end_year + 1): # We add 1 to start_year cause start_year of eco2mix is an empty year. 
-            year_of_data = fetch_data_from_api(endpoint="/exports/parquet").content
-            with open("data/eco2mix-regional-cons-def"+str(curr_year)+".parquet", "wb") as wb:
-                wb.write(year_of_data)
+    @task
+    def compute_years() -> list[int]:
+        boundaries = retrieve_boundaries_years()
+        return list(range(boundaries["start_year"] + 1, boundaries["end_year"] + 1)) # We add 1 to start_year cause start_year of eco2mix is an empty year.
 
-    extract_data_from_eco2mix()
+    @task(max_active_tis_per_dagrun=1)
+    def fetch_year(year: int):
+        where = f"year(date_heure) = {year}"
+        print(where)
+        year_of_data = fetch_data_from_api(endpoint="/exports/parquet", where=where)
+        with open(f"data/eco2mix-regional-cons-def{str(year)}.parquet", "wb") as wb:
+            wb.write(year_of_data.content)
+
+    years = compute_years()
+    fetch_year.expand(year=years)
+
 
 simple_extraction()
